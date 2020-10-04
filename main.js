ac1d36fe17ba320bcae2e9905cc8d1af77609dfb
Idan Moral <idan22moral@gmail.com>
2020-10-05 00:31:55 +0300

    Room creation implementation and improvements

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const http = require('http');
-const server = http.createServer();
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const server = app.listen(80, () => { console.log('Listening...') });
 const io = require('socket.io')(server);
 
 var rooms = {};
@@ -20,17 +22,22 @@ function generateRoomId() {
 
 io.on('connection', (socket) => {
     socket.on('create-room', async (username) => {
+        username = username.trim();
+
         // user has no existing rooms
-        if (Object.keys(socket.rooms).length == 1 && socket.rooms == socket.id) {
+        if (Object.keys(socket.rooms).length == 1 && username.match(/^[^@#]+$/)) {
             var roomId = generateRoomId();
 
-            // create the room
-            var newRoom = rooms[roomId] = {
+            // save room details
+            rooms[roomId] = {
                 'admin': {
                     'username': username,
                     'socket': socket
                 }
             };
+
+            // add the user to the new room
+            socket.join(roomId);
 
             // send the room ID
             socket.emit('room', {
@@ -43,5 +50,3 @@ io.on('connection', (socket) => {
         }
     });
 });
-
-io.listen(3000);
--- public/index.js
+++ public/index.js
@@ -16,12 +16,49 @@ $('[required]').on('invalid', (e) => {
 
 $('#create-btn').on('click', (e) => {
     if ($('#main-form')[0].checkValidity()) {
-        // move the user's view to the game
-        $.toast({
-            type: 'success',
-            title: 'Yay!',
-            content: 'Room created successfully!',
-            delay: 5000
-        });
+        initLobbyFrame(true);
     }
+});
+
+function switchFrame(targetFrame) {
+    // hide all existing frames
+    $('[frame]').not(`#${targetFrame}`).hide();
+
+    // show target frame
+    $(`#${targetFrame}`).show();
+}
+
+function initLobbyFrame(isRoomAdmin) {
+    window.serverSocket = io();
+
+    if (isRoomAdmin) {
+        window.serverSocket.on('room', (details) => {
+            if (details.status == 'success') {
+                switchFrame('lobby-frame');
+                $.toast({
+                    type: 'success',
+                    title: 'Yay!',
+                    content: 'Room created successfully!',
+                    delay: 5000
+                });
+            } else {
+                $.toast({
+                    type: 'error',
+                    title: 'Oh...',
+                    content: 'Failed to create a room.',
+                    delay: 5000
+                });
+                window.serverSocket.disconnect();
+                window.serverSocket = null;
+            }
+        });
+
+        window.serverSocket.emit('create-room', $('#nickname-input').val());
+    } else {
+        // lobby frame initialization in case of 'Join Room'.
+    }
+}
+
+$('#exit-btn').on('click', (e) => {
+    window.serverSocket.disconnect();
 });
